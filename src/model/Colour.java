ae55a3b0e5a573a6a86215f43a17abe39db8719d
Abdelrhman-Essam497 <abdelrhmanessam497@gmail.com>
2025-02-28 17:38:10 +0200

    Added the Colour enum and marble Class

---
+++ src/model/Colour.java
@@ -0,0 +1,6 @@
+package model;
+
+public enum Colour{
+    RED,GREEN,BLUE,YELLOW
+}
+
---
+++ src/model/player/marble.java
@@ -0,0 +1,13 @@
+package model.player;
+import model;
+public class marble {
+    private final Colour colour;
+
+    public marble(Colour colour) {
+        this.colour = colour;
+    }
+
+private getColour()
+{return this.colour;}
+
+}
