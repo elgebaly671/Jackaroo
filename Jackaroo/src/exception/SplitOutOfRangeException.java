b65894a15ccd89e700878ea7e9d80f07c45130e1
Youssef-elwa7sh <youssef2742006@gmail.com>
2025-03-01 17:00:03 +0200

    Update SplitOutOfRangeException.java

--- Jackaroo/src/exception/SplitOutOfRangeException.java
+++ Jackaroo/src/exception/SplitOutOfRangeException.java
@@ -2,7 +2,7 @@ package exception;
 
 public class SplitOutOfRangeException extends InvalidSelectionException {
     public SplitOutOfRangeException() {
-        super("Split distance is out of range.");
+        super("Split out of range.");
     }
 
     public SplitOutOfRangeException(String message) {
